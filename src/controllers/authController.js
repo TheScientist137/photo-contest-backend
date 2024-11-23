905f8599e0a8ddaad4cf4189e1c539719deeee93
The-Scientist <thescientist137@outlook.es>
2024-11-23 11:37:25 +0100

    created login succesfully using bcrypt and jsowebtoken to manage the user session succesfully -- tested on postman

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const { UserModel } = require('../db/models');
 
 // Controller to register a new user
@@ -6,22 +7,41 @@ const registerUser = async (req, res) => {
  try {
   const { username, email, password } = req.body;
 
-  const existingUser = await UserModel.findOne({ where: { username, email } });
-  if (existingUser) {
+  const user = await UserModel.findOne({ where: { email } });
+  if (user) {
    return res.status(400).json({ message: 'Username or email already in use' });
   }
 
+  // Hash password with bcrypt
   const hashedPassword = await bcrypt.hash(password, 10);
 
   const newUser = await UserModel.create({ username, email, password: hashedPassword });
-  res.status(201).json(newUser);
+  res.status(201).json({ message: 'User registered successfully', newUser });
  } catch (error) {
     res.status(500).json({ message: 'Failed to register a new user', error });
    }
 }
 
 // Controller to log in
+const logInUser = async (req, res) => {
+ try {
+  const { email, password } = req.body;
+  const user = await UserModel.findOne({ where: { email } });
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  
+  if (!user || !isPasswordValid) {
+   return res.status(400).json({ message: 'Invalid email or password' });
+  }
+
+  // Generate a JWT
+  const token = jwt.sign({ id: user.id, email: user.email }, 'secret', { expiresIn: '1h' });
+
+  res.status(200).json({ message: 'Log in succesfully', token, user });
+ } catch (error) {
+    res.status(500).json({ message: 'Error in log in', error });
+ }
+}
 
 // Controller to log out
 
-module.exports = { registerUser };
+module.exports = { registerUser, logInUser };
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser } = require('../controllers/authController');
+const { registerUser, logInUser } = require('../controllers/authController');
 
 // Route to register a new user
 router.post('/register', registerUser);
+
+// Route to log in
+router.post('/login', logInUser);
+
+// Route to log out
 
 module.exports = router;
